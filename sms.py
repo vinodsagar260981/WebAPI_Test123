633f6533db309b975ba1e52eba176315f975d758
Vinod Sagar <vinodsagar@Vinods-MacBook-Pro.local>
2021-09-02 15:45:42 +0530

    adding starting project

---
+++ sms.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client 
+ 
+account_sid = 'AC4052a342600ed0abb67b8dc8561cbcec' 
+auth_token = '4512b1fc15e9e15c0e307d0660a9f9ce' 
+client = Client(account_sid, auth_token) 
+ 
+message = client.messages.create(  
+                              messaging_service_sid='MG3211d5784b97a33b6a689ddeca7ced4f', 
+                              body='I am finding odd',      
+                              to='+919591450709' 
+                          ) 
+ 
+print(message.sid)
